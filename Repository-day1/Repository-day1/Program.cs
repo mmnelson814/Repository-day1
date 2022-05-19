a2d1b584426565afc233a813de2927d57c5d21a0
Xavier M <golf4Xavier@gmail.com>
2022-05-19 16:02:04 -0400

    adding conditional

--- Repository-day1/Repository-day1/Program.cs
+++ Repository-day1/Repository-day1/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Mmel and xavier's Guessing game!");
+int puppies = 8;
+Console.WriteLine("Please guess a number bettween 1-10.");
+string input = Console.ReadLine();
+int inputNum = Convert.ToInt32(input);
+if(inputNum == puppies)
+{
+    Console.WriteLine("Number is correct");
+}
+else if (inputNum > 10 || inputNum < 1)
+{
+    Console.WriteLine("User must use only 1-10");
+}
+else
+{
+    Console.WriteLine("number is not correct. Try Again!");
+}

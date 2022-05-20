47a4b209e7b0748ec831190056e4c5fbf8f11b3d
Xavier M <golf4Xavier@gmail.com>
2022-05-20 14:26:43 -0400

    adding guessing game part 3

--- Repository-day1/Repository-day1/Program.cs
+++ Repository-day1/Repository-day1/Program.cs
@@ -16,3 +16,67 @@ else
 {
     Console.WriteLine("number is not correct. Try Again!");
 }
+
+//guessing game
+Console.WriteLine("Please guess a random number.");
+string inputR = Console.ReadLine();
+int inVal = Convert.ToInt32(inputR);
+int right = 12;
+if (inVal > right || inVal < right)
+{
+    Console.WriteLine(inVal - right + " is the difference from the correct answer.");
+    Console.WriteLine("Please guess a random number.");
+    string inputR2 = Console.ReadLine(); 
+    inVal = Convert.ToInt32(inputR2);
+    if (inVal == right)
+    {
+        Console.WriteLine("You have choosen the right answer");
+    }
+    else
+    {
+        Console.WriteLine(inVal - right + " is the difference from the correct answer.");
+        Console.WriteLine("Please guess a random number.");
+        string inputR3 = Console.ReadLine();
+        inVal = Convert.ToInt32(inputR3);
+    }
+
+}
+else
+{
+    Console.WriteLine("You have choosen the right answer");
+}
+
+//random number gen
+Console.WriteLine("Please guess a random number.");
+string inputRD = Console.ReadLine();
+int inValR = Convert.ToInt32(inputR);
+int rightR = 12;
+if (inValR > rightR || inValR < rightR)
+{
+    Random inValN = new Random();
+    for (int i = 0; i < 4; i++)
+    {
+        inValN.Next(10);
+    }
+    int inValNumR = Convert.ToInt32(inValN);
+    Console.WriteLine(inValNumR - rightR + " is the difference from the correct answer.");
+    Console.WriteLine("Please guess a random number.");
+    string inputRD2 = Console.ReadLine();
+    inValR = Convert.ToInt32(inputRD2);
+    if (inValR == rightR)
+    {
+        Console.WriteLine("You have choosen the right answer");
+    }
+    else
+    {
+        Console.WriteLine(inValNumR - rightR + " is the difference from the correct answer.");
+        Console.WriteLine("Please guess a random number.");
+        string inputRD3 = Console.ReadLine();
+        inValR = Convert.ToInt32(inputRD3);
+    }
+
+}
+else
+{
+    Console.WriteLine("You have choosen the right answer");
+}
